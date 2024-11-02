60c7bbe0c25841285808b9f621d62f160cf7c490
Ayaz Tirpanci <ayztrpnc@outlook.com>
2024-11-02 01:35:46 +0000

    api get requests and sql database

---
+++ backend/Dump20241102.sql
@@ -0,0 +1,192 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: hack_a_duck
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `booking`
+--
+
+DROP TABLE IF EXISTS `booking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `booking` (
+  `bookingID` int NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL,
+  `deskID` int NOT NULL,
+  `bookingStart` datetime NOT NULL,
+  `bookingEnd` datetime NOT NULL,
+  `bookingDesc` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`bookingID`),
+  KEY `userID` (`userID`),
+  KEY `deskID` (`deskID`),
+  CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`),
+  CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`deskID`) REFERENCES `desk` (`deskID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking`
+--
+
+LOCK TABLES `booking` WRITE;
+/*!40000 ALTER TABLE `booking` DISABLE KEYS */;
+/*!40000 ALTER TABLE `booking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `desk`
+--
+
+DROP TABLE IF EXISTS `desk`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `desk` (
+  `deskID` int NOT NULL AUTO_INCREMENT,
+  `roomID` int NOT NULL,
+  `deskName` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`deskID`),
+  KEY `roomID` (`roomID`),
+  CONSTRAINT `desk_ibfk_1` FOREIGN KEY (`roomID`) REFERENCES `room` (`roomID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `desk`
+--
+
+LOCK TABLES `desk` WRITE;
+/*!40000 ALTER TABLE `desk` DISABLE KEYS */;
+INSERT INTO `desk` VALUES (1,1,'Feather Frenzy'),(2,1,'Nesting Desk');
+/*!40000 ALTER TABLE `desk` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `floor`
+--
+
+DROP TABLE IF EXISTS `floor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `floor` (
+  `floorID` int NOT NULL AUTO_INCREMENT,
+  `floorName` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`floorID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `floor`
+--
+
+LOCK TABLES `floor` WRITE;
+/*!40000 ALTER TABLE `floor` DISABLE KEYS */;
+INSERT INTO `floor` VALUES (1,'Duck Yard'),(2,'Quack Tower'),(3,'Feather Heights');
+/*!40000 ALTER TABLE `floor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `roleID` int NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`roleID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'User');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room` (
+  `roomID` int NOT NULL AUTO_INCREMENT,
+  `floorID` int NOT NULL,
+  `roomName` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`roomID`),
+  KEY `floorID` (`floorID`),
+  CONSTRAINT `room_ibfk_1` FOREIGN KEY (`floorID`) REFERENCES `floor` (`floorID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` VALUES (1,1,'Quack Command Center'),(2,1,'Quiet Quacker'),(3,1,'Splash Zone');
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `roleID` int NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `pword` varchar(255) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `email` (`email`),
+  KEY `roleID` (`roleID`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`roleID`) REFERENCES `role` (`roleID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,1,'root','root','root','root','root@rootmail.com');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-02  1:30:16
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const connection = require("./db")
 
 // Middleware to parse JSON
 app.use(express.json());
 
-// Sample route
-app.get('/api/test', (req, res) => {
-    res.json({ message: 'Hello from Node.js backend!' });
-});
-
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    connection.connect(function(err){
+        if(err) {throw err;}
+        console.log("Database connected.")
+    })
 });
+
+const userRouter = require("./routes/users")
+const floorRouter = require("./routes/floors")
+const roleRouter = require("./routes/roles")
+const roomRouter = require("./routes/rooms")
+const deskRouter = require("./routes/desks")
+app.use("/users", userRouter)
+app.use("/floors", floorRouter)
+app.use("/roles", roleRouter)
+app.use("/rooms", roomRouter)
+app.use("/desks", deskRouter)
